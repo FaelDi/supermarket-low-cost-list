27e365f02cf0f88b868801f3f4275958c150d139
FaelDi <faeldii@icloud.com>
2021-08-15 16:41:06 -0300

    starter project

---
+++ src/main/java/com/wordpress/faeldi/supermarketlowcostlist/SupermarketLowCostListApplication.java
@@ -0,0 +1,13 @@
+package com.wordpress.faeldi.supermarketlowcostlist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupermarketLowCostListApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SupermarketLowCostListApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wordpress/faeldi/supermarketlowcostlist/SupermarketLowCostListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wordpress.faeldi.supermarketlowcostlist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SupermarketLowCostListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
